b0170639aa2284df33d84999b19e6cf76cebd036
Khoa hoc du lieu 17a3 <ntkhoi.dhkl17a3hn@sv.uneti.edu.vn>
2024-01-12 10:52:23 +0700

    add file

---
+++ 11.1.py
@@ -0,0 +1,7 @@
+def chuong11_bai1():
+ a=[1,2,3]
+ b=[1,[2,3]]
+ c=[]
+ d=[1,2,3][1:]
+ print(len(a),len(b),len(c),len(d))
+ 
---
+++ 11.8.py
@@ -0,0 +1,6 @@
+nums=[1,2,3,4,7,5,6,8,9]
+def has_lucky_numbers(nums):
+     for i in nums:
+        if i%7==0:
+            print(True)
+            break 
